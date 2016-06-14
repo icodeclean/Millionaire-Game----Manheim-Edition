912764975be6595a0a957bf14f732373dd488631
Federik Codelas <icodeclean@gmail.com>
2016-06-13 20:17:36 -0400

    Game Menu

--- millionaire.rb
+++ millionaire.rb
@@ -35,12 +35,27 @@ class Millionaire
     title_screen()
     @player = get_user_input("\nWelcome to Who wants to be a Millionaire! What is your name?")
     title_screen()
-    print "\nThank You #{@player}! "
+    puts "Thank You #{@player}!"
     sleep(1)
+  end
+
+  def menu()
+    title_screen
+    user_choice = get_user_input("#{@player}, what would you like to do?:\n 1 - Start Game \t 2 - Read Instructions")
+      if (user_choice  == '1')
+        puts " Play Game"
+      elsif (user_choice  == '2')
+        puts "Instructions"
+      else
+        title_screen
+        puts "\nInvalid input, Please answer 1 to Start Game or 2 to Read Instructions"
+        menu()
+      end
   end
 
   def play()
     get_player_name()
+    menu() 
   end
 
 end
