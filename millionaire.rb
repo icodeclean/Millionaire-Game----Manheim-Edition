7ba6643054ecf96d19adc89573d09fdfb32a57da
Federik Codelas <icodeclean@gmail.com>
2016-06-13 20:06:56 -0400

    Request Player Name

--- millionaire.rb
+++ millionaire.rb
@@ -17,12 +17,30 @@ class Millionaire
     puts "*" * 50
   end
 
+  def prompt()
+    print '> '
+  end
+
   def clear()
     system "clear" or system "cls"
   end
 
-  def play()
+  def get_user_input(question)
+    puts question
+    prompt()
+    return gets.chomp()
+  end
+
+  def get_player_name()
     title_screen()
+    @player = get_user_input("\nWelcome to Who wants to be a Millionaire! What is your name?")
+    title_screen()
+    print "\nThank You #{@player}! "
+    sleep(1)
+  end
+
+  def play()
+    get_player_name()
   end
 
 end
