1bf91cd363430f01941539dece58e02f19566559
Federik Codelas <icodeclean@gmail.com>
2016-06-14 19:54:28 -0400

    Put meat on Game Sequence. Almost MVP

--- millionaire.rb
+++ millionaire.rb
@@ -1,10 +1,44 @@
 class Millionaire 
+  
+  attr_reader :current_question, :current_prize, :continue 
+  attr_writer :current_question, :current_prize, :continue 
 
   def initialize()
     @player = ''
     @current_question = 0
     @continue = true
+    @prize_for_questions = [100, 200, 300, 500, 1000, 2000, 4000, 8000, 16000, 32000, 64000, 125000, 250000, 500000, 1000000]
+    @current_prize = 0
+    
+    @data_bank = [
+    ['What kind of flytrap is named for a carnivorous animal?', 'A: Jupiter', 'B: Pluto', 'C: Mars', 'D: Venus', 'D'],
+    ['What kind of flytrap is named for a carnivorous animal?', 'A: Jupiter', 'B: Pluto', 'C: Mars', 'D: Venus', 'D'],
+    ['What kind of flytrap is named for a carnivorous animal?', 'A: Jupiter', 'B: Pluto', 'C: Mars', 'D: Venus', 'D'],
+    ['What kind of flytrap is named for a carnivorous animal?', 'A: Jupiter', 'B: Pluto', 'C: Mars', 'D: Venus', 'D'],
+    ['What kind of flytrap is named for a carnivorous animal?', 'A: Jupiter', 'B: Pluto', 'C: Mars', 'D: Venus', 'D'],
+    ['What kind of flytrap is named for a carnivorous animal?', 'A: Jupiter', 'B: Pluto', 'C: Mars', 'D: Venus', 'D'],
+    ['What kind of flytrap is named for a carnivorous animal?', 'A: Jupiter', 'B: Pluto', 'C: Mars', 'D: Venus', 'D'],
+    ['What kind of flytrap is named for a carnivorous animal?', 'A: Jupiter', 'B: Pluto', 'C: Mars', 'D: Venus', 'D'],
+    ['What kind of flytrap is named for a carnivorous animal?', 'A: Jupiter', 'B: Pluto', 'C: Mars', 'D: Venus', 'D'],
+    ['What kind of flytrap is named for a carnivorous animal?', 'A: Jupiter', 'B: Pluto', 'C: Mars', 'D: Venus', 'D'],
+    ['What kind of flytrap is named for a carnivorous animal?', 'A: Jupiter', 'B: Pluto', 'C: Mars', 'D: Venus', 'D'],
+    ['What kind of flytrap is named for a carnivorous animal?', 'A: Jupiter', 'B: Pluto', 'C: Mars', 'D: Venus', 'D'],
+    ['What kind of flytrap is named for a carnivorous animal?', 'A: Jupiter', 'B: Pluto', 'C: Mars', 'D: Venus', 'D'],
+    ['What kind of flytrap is named for a carnivorous animal?', 'A: Jupiter', 'B: Pluto', 'C: Mars', 'D: Venus', 'D'],
+    ['What kind of flytrap is named for a carnivorous animal?', 'A: Jupiter', 'B: Pluto', 'C: Mars', 'D: Venus', 'D'],
+    ['What kind of flytrap is named for a carnivorous animal?', 'A: Jupiter', 'B: Pluto', 'C: Mars', 'D: Venus', 'D'],
+    ['What kind of flytrap is named for a carnivorous animal?', 'A: Jupiter', 'B: Pluto', 'C: Mars', 'D: Venus', 'D'],
+    ['What kind of flytrap is named for a carnivorous animal?', 'A: Jupiter', 'B: Pluto', 'C: Mars', 'D: Venus', 'D'],
+    ['What kind of flytrap is named for a carnivorous animal?', 'A: Jupiter', 'B: Pluto', 'C: Mars', 'D: Venus', 'D'],
+    ['What kind of flytrap is named for a carnivorous animal?', 'A: Jupiter', 'B: Pluto', 'C: Mars', 'D: Venus', 'D'],
+    ['What kind of flytrap is named for a carnivorous animal?', 'A: Jupiter', 'B: Pluto', 'C: Mars', 'D: Venus', 'D'],
+    ['What kind of flytrap is named for a carnivorous animal?', 'A: Jupiter', 'B: Pluto', 'C: Mars', 'D: Venus', 'D'],
+    ['What kind of flytrap is named for a carnivorous animal?', 'A: Jupiter', 'B: Pluto', 'C: Mars', 'D: Venus', 'D'],
+                   ]
+
+    @selected_questions = randomize(@data_bank)               
   end  
+
 
   def title_screen()
     clear()
@@ -31,6 +65,10 @@ class Millionaire
     system "clear" or system "cls"
   end
 
+  def randomize(data_bank)
+    return data_bank.shuffle.take(15)
+  end
+
   def get_user_input(question)
     puts question
     prompt()
@@ -39,7 +77,7 @@ class Millionaire
 
   def get_player_name()
     title_screen()
-    @player = get_user_input("\nWelcome to Who wants to be a Millionaire! What is your name?")
+    @player = get_user_input("\nWelcome to Who wants to be a Millionaire! What is your name?").capitalize
     title_screen()
     puts "Thank You #{@player}!"
     sleep(1)
@@ -53,9 +91,10 @@ class Millionaire
   end  
 
   def menu()
-    title_screen
+    title_screen()
     user_choice = get_user_input("#{@player}, what would you like to do?\n 1 - Start Game \t 2 - Read Instructions")
       if (user_choice  == '1')
+        title_screen()
         start_game()
       elsif (user_choice  == '2')
         instructions()
@@ -68,12 +107,58 @@ class Millionaire
   end
   
   def present_question()
-    puts "Question is presented"
+    current_question_set = @selected_questions[@current_question]
+    correct_answer =  current_question_set[5]
+    puts "\n\n"
+    puts current_question_set[0] + "\n" + current_question_set[1] + "\t" + current_question_set[2] + "\n" + current_question_set[3] + "\t \t" + current_question_set[4]
+    check_answer(correct_answer)
+    continue()  
+  end
+
+  def continue()
+    decision = get_user_input("\n\n#{@player}, would you like to:\n1 - Continue  2 - Leave with $#{@current_prize}?")
+      if (decision == '1')
+        @continue = true
+        game_sequence()
+      elsif (decision == '2')
+        leave()
+      else
+        puts "\n***   Please select 1 or 2   ***"
+        continue()
+      end  
+  end
+
+  def check_answer(correct_answer)
+    player_response = get_user_input('').upcase 
+      if player_response == correct_answer
+        award_prize()
+      else
+        
+      end
+  end
+
+  def award_prize()
+    @current_prize = @prize_for_questions[@current_question]
+    grand_prize() if @current_prize == 1000000
+    puts "\n Well done #{@player}, You have Won $#{@current_prize}"
     @current_question += 1
+  end
+
+  def grand_prize()
+    title_screen
+    puts "You just won a Million Smackarooos\n\n\n!!!"
+    exit
+  end  
+
+  def leave()
+    title_screen()
+    puts "\n\nYou have Chosen to leave with $#{@current_prize}"
+    puts "Well done #{@player}, Good luck to you! \n\n\n"
+    @continue = false
   end
   
   def game_sequence()
-    while (@continue && @current_question <= 3) do
+    while (@continue == true && @current_question <= 3) do
       present_question()
     end      
   end
